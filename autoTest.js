63d607c6bf0f8b5a42bfd9ad0a273df6c0c8e21e
Anandu P R <ananduprajesh@gmail.com>
2022-05-22 12:05:09 +0530

    auto server quering file added

--- autoTest.js
+++ autoTest.js
@@ -8,23 +8,24 @@ const osPlatform = platform();
 const args = process.argv.slice(2);
 const [url] = args;
 const callServer = () => {
-    if (url === undefined) {
-        console.error('Please enter a URL, e.g. "http://www.opencanvas.co.uk"');
-        process.exit(0);
-    }
-
-    let command;
-
-    if (osPlatform === WINDOWS_PLATFORM) {
-        command = `start microsoft-edge:${url}`;
-    } else if (osPlatform === MAC_PLATFORM) {
-        command = `open -a "Google Chrome" ${url}`;
-    } else {
-        command = `google-chrome --no-sandbox ${url}`;
-    }
+    // if (url === undefined) {
+    //     console.error('Please enter a URL, e.g. "http://www.opencanvas.co.uk"');
+    //     process.exit(0);
+    // }
 
     console.log(`\n Running Script to send request\nExecuting command: ${command}\n`);
+    
+    let command = `ftp cryptozite.herokuapp.com`
+
+    // if (osPlatform === WINDOWS_PLATFORM) {
+    //     command = `start microsoft-edge:${url}`;
+    // } else if (osPlatform === MAC_PLATFORM) {
+    //     command = `open -a "Google Chrome" ${url}`;
+    // } else {
+    //     command = `google-chrome --no-sandbox ${url}`;
+    // }
+
 
     exec(command);
 }
-setTimeout(callServer,6*60*60*1000)
+setTimeout(callServer, 6 * 60 * 60 * 1000)
